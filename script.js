926bd8a9f1ce9f4dedd3e4e40cbcfc18f1aada26
Dimm005 <dimm005@gmail.com>
2024-10-01 08:54:12 +0800

    Fix bug with empty library Fix the bug when JS script stopped with empty (new) library

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var myLibrary = []; // array to store book objects
 var currentLibrary = []; // keeps current state of shown library (all, read or not etc.)
-var isList = false; // should book view be shown as list or as set of cards (default)
+// var isList = false; // should book view be shown as list or as set of cards (default)
 
 // book object constructor
 function Book(title, author, pages) {
@@ -24,6 +24,9 @@ function saveLibrary() {
 // read library from localStorage
 function readLibrary() {
     myLibrary = JSON.parse(localStorage.getItem("library"));
+    if (! myLibrary) {
+        myLibrary = [];
+    };
 }
 
 // add book to the library
@@ -36,116 +39,116 @@ function addBookToLibrary(title, author, pages) {
     fillBooksList(currentLibrary);
 }
 
-// create book line element
-function createBookLineElement(book) {
-    let bookList = document.getElementById("book-list");
-    let bookLine = document.createElement("div");
-    bookLine.classList.add("book-element");
-    bookLine.classList.add("line");
-    bookLine.setAttribute("id", book.dateAdded);
-    bookList.appendChild(bookLine);
-    let bookDescription = `"${book.title}" by ${book.author}, ${book.pages} pages, ${book.isRead ? "read already" : "not read yet"}`;
-    let para = document.createElement("p");
-    para.innerText = bookDescription;
-    bookLine.appendChild(para);
+// // create book line element
+// function createBookLineElement(book) {
+//     let bookList = document.getElementById("book-list");
+//     let bookLine = document.createElement("div");
+//     bookLine.classList.add("book-element");
+//     bookLine.classList.add("line");
+//     bookLine.setAttribute("id", book.dateAdded);
+//     bookList.appendChild(bookLine);
+//     let bookDescription = `"${book.title}" by ${book.author}, ${book.pages} pages, ${book.isRead ? "read already" : "not read yet"}`;
+//     let para = document.createElement("p");
+//     para.innerText = bookDescription;
+//     bookLine.appendChild(para);
 
-    // add "Mark as read", "Edit" and "Remove" buttons
-    const markReadButton = document.createElement("button");
-    markReadButton.classList.add("mark-read-button");
-    markReadButton.setAttribute("id", ("mark-read-" + book.dateAdded));
-    markReadButton.innerText = "Mark as read";
-    bookLine.appendChild(markReadButton);
-    markReadButton.addEventListener("click", (e) => {
-        let bookId = getBookId(e.target.id);
-        for (let i = 0; i < myLibrary.length; i++) {
-            if (myLibrary[i].dateAdded.toString() === bookId) {
-                myLibrary[i].isRead = true;
-                saveLibrary();
-                clearBooksList();
-                currentLibrary = sortByTitle(arrayDeepCopy(myLibrary));
-                fillBooksList(currentLibrary);
-                break;
-            };
-        };
-    })
+//     // add "Mark as read", "Edit" and "Remove" buttons
+//     const markReadButton = document.createElement("button");
+//     markReadButton.classList.add("mark-read-button");
+//     markReadButton.setAttribute("id", ("mark-read-" + book.dateAdded));
+//     markReadButton.innerText = "Mark as read";
+//     bookLine.appendChild(markReadButton);
+//     markReadButton.addEventListener("click", (e) => {
+//         let bookId = getBookId(e.target.id);
+//         for (let i = 0; i < myLibrary.length; i++) {
+//             if (myLibrary[i].dateAdded.toString() === bookId) {
+//                 myLibrary[i].isRead = true;
+//                 saveLibrary();
+//                 clearBooksList();
+//                 currentLibrary = sortByTitle(arrayDeepCopy(myLibrary));
+//                 fillBooksList(currentLibrary);
+//                 break;
+//             };
+//         };
+//     })
 
-    const editButton = document.createElement("button");
-    editButton.classList.add("edit-button");
-    editButton.setAttribute("id", ("edit-" + book.dateAdded));
-    editButton.innerText = "Edit book";
-    bookLine.appendChild(editButton);
-    editButton.addEventListener("click", (e) => {
-        let editDialog = document.getElementById("edit-book-dialog");
-        editDialog.showModal();
-        editTitle = document.getElementById("edit-title");
-        editTitle.value = book.title;
-        editAuthor = document.getElementById("edit-author");
-        editAuthor.value = book.author;
-        editPages = document.getElementById("edit-pages");
-        editPages.value = book.pages;
-        editNotRead = document.getElementById("edit-not-read");
-        editRead = document.getElementById("edit-read");
-        if (book.isRead === true) {
-            editRead.setAttribute("checked", "checked");
-        } else {
-            editNotRead.setAttribute("checked", "checked");
-        };
-        let editCancelBtn = document.getElementById("cancel-edit");
-        editCancelBtn.addEventListener("click", () => {
-            editDialog.close();
-        });
-        let editSaveBtn = document.getElementById("save-edit");
-        editSaveBtn.addEventListener("click", () => {
-            for (let i = 0; i < myLibrary.length; i++) {
-                if (myLibrary[i].dateAdded === book.dateAdded) {
-                    myLibrary[i].title = editTitle.value;
-                    myLibrary[i].author = editAuthor.value;
-                    myLibrary[i].pages = editPages.value;
-                    if (editRead.checked === true) {
-                        myLibrary[i].isRead = true;
-                    } else {
-                        myLibrary[i].isRead = false;
-                    };
-                    saveLibrary();
-                    clearBooksList();
-                    currentLibrary = sortByTitle(arrayDeepCopy(myLibrary));
-                    fillBooksList(currentLibrary);
-                    editDialog.close();
-                    break;
-                };
-            };
-        });
-    })
+//     const editButton = document.createElement("button");
+//     editButton.classList.add("edit-button");
+//     editButton.setAttribute("id", ("edit-" + book.dateAdded));
+//     editButton.innerText = "Edit book";
+//     bookLine.appendChild(editButton);
+//     editButton.addEventListener("click", (e) => {
+//         let editDialog = document.getElementById("edit-book-dialog");
+//         editDialog.showModal();
+//         editTitle = document.getElementById("edit-title");
+//         editTitle.value = book.title;
+//         editAuthor = document.getElementById("edit-author");
+//         editAuthor.value = book.author;
+//         editPages = document.getElementById("edit-pages");
+//         editPages.value = book.pages;
+//         editNotRead = document.getElementById("edit-not-read");
+//         editRead = document.getElementById("edit-read");
+//         if (book.isRead === true) {
+//             editRead.setAttribute("checked", "checked");
+//         } else {
+//             editNotRead.setAttribute("checked", "checked");
+//         };
+//         let editCancelBtn = document.getElementById("cancel-edit");
+//         editCancelBtn.addEventListener("click", () => {
+//             editDialog.close();
+//         });
+//         let editSaveBtn = document.getElementById("save-edit");
+//         editSaveBtn.addEventListener("click", () => {
+//             for (let i = 0; i < myLibrary.length; i++) {
+//                 if (myLibrary[i].dateAdded === book.dateAdded) {
+//                     myLibrary[i].title = editTitle.value;
+//                     myLibrary[i].author = editAuthor.value;
+//                     myLibrary[i].pages = editPages.value;
+//                     if (editRead.checked === true) {
+//                         myLibrary[i].isRead = true;
+//                     } else {
+//                         myLibrary[i].isRead = false;
+//                     };
+//                     saveLibrary();
+//                     clearBooksList();
+//                     currentLibrary = sortByTitle(arrayDeepCopy(myLibrary));
+//                     fillBooksList(currentLibrary);
+//                     editDialog.close();
+//                     break;
+//                 };
+//             };
+//         });
+//     })
 
 
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("remove-button");
-    removeButton.setAttribute("id", ("remove-" + book.dateAdded));
-    removeButton.innerText = "Remove book";
-    bookLine.appendChild(removeButton);
-    removeButton.addEventListener("click", (e) => {
-        let removeBookDialog = document.getElementById("remove-book-dialog");
-        removeBookDialog.showModal();
-        let removeBookNoBtn = document.getElementById("remove-book-no");
-        let removeBookYesBtn = document.getElementById("remove-book-yes");
-        removeBookNoBtn.addEventListener("click", () => {
-            removeBookDialog.close();
-        });
-        removeBookYesBtn.addEventListener("click", (e) => {
-            for (let i = 0; i < myLibrary.length; i++) {
-                if (myLibrary[i].dateAdded.toString() === bookLine.id) {
-                    myLibrary.splice(i, 1);
-                    saveLibrary();
-                    clearBooksList();
-                    currentLibrary = sortByTitle(arrayDeepCopy(myLibrary));
-                    fillBooksList(currentLibrary);
-                    removeBookDialog.close();
-                    break;
-                };
-            };
-            })
-    })
-}
+//     const removeButton = document.createElement("button");
+//     removeButton.classList.add("remove-button");
+//     removeButton.setAttribute("id", ("remove-" + book.dateAdded));
+//     removeButton.innerText = "Remove book";
+//     bookLine.appendChild(removeButton);
+//     removeButton.addEventListener("click", (e) => {
+//         let removeBookDialog = document.getElementById("remove-book-dialog");
+//         removeBookDialog.showModal();
+//         let removeBookNoBtn = document.getElementById("remove-book-no");
+//         let removeBookYesBtn = document.getElementById("remove-book-yes");
+//         removeBookNoBtn.addEventListener("click", () => {
+//             removeBookDialog.close();
+//         });
+//         removeBookYesBtn.addEventListener("click", (e) => {
+//             for (let i = 0; i < myLibrary.length; i++) {
+//                 if (myLibrary[i].dateAdded.toString() === bookLine.id) {
+//                     myLibrary.splice(i, 1);
+//                     saveLibrary();
+//                     clearBooksList();
+//                     currentLibrary = sortByTitle(arrayDeepCopy(myLibrary));
+//                     fillBooksList(currentLibrary);
+//                     removeBookDialog.close();
+//                     break;
+//                 };
+//             };
+//             })
+//     })
+// }
 
 // create book card element
 function createBookCardElement(book) {
@@ -260,11 +263,7 @@ function createBookCardElement(book) {
 // fill books list depending on isList
 function fillBooksList(library) {
     for (let book of library) {
-        if (isList) {
-            createBookLineElement(book);
-        } else {
-            createBookCardElement(book);
-        };
+        createBookCardElement(book);
     };
 }
 
@@ -376,17 +375,17 @@ removeAllBtn.addEventListener("click", () => {
     })  
 })
 
-//add event listeners to view options
-const viewOption = document.getElementById("view");
-viewOption.addEventListener("change", (e) => {
-    clearBooksList();
-    if (e.target.value === "cards") {
-        isList = false;
-    } else {
-        isList = true;
-    };
-    fillBooksList(currentLibrary);
-});
+// //add event listeners to view options
+// const viewOption = document.getElementById("view");
+// viewOption.addEventListener("change", (e) => {
+//     clearBooksList();
+//     if (e.target.value === "cards") {
+//         isList = false;
+//     } else {
+//         isList = true;
+//     };
+//     fillBooksList(currentLibrary);
+// });
 
 // add event listener to sorting options
 const sortOption = document.getElementById("sort");
